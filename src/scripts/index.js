540bf6bf4bded0d627781b82c91c8cdad7fd471d
Ana Ferreira <anapaulaferreira0309@gmail.com>
2022-04-23 19:10:49 -0300

    Corrigindo ação do botão para descriptografar

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,27 +2,41 @@ import encrypt from './encrypt.js';
 import decrypt from './decrypt.js';
 
 const DOM = {
-  textarea: document.querySelector('#user_text'),
+  userText: document.querySelector('#user_text'),
   btnEncrypt: document.querySelector('.btn.encrypt'),
   btnDecrypt: document.querySelector('.btn.decrypt'),
   btnDelete: document.querySelector('.btn.delete'),
   btnCopy: document.querySelector('.btn.copy'),
+  displaySearching: document.querySelector('section.display .display_searching'),
+  displayResult: document.querySelector('section.display .display_result'),
+  displayResultText: document.querySelector('#display_result_text'),
 };
 
+function showMessage(text) {
+  DOM.displaySearching.classList.remove('active');
+  DOM.displayResult.classList.add('active');
+  DOM.displayResultText.value = text;
+}
+
 DOM.btnEncrypt.addEventListener('click', () => {
-  if (DOM.textarea.value.trim()) {
-    let text = DOM.textarea.value.toLowerCase();
-    encrypt(text);
+  if (DOM.userText.value.trim()) {
+    let text = DOM.userText.value.toLowerCase();
+    showMessage(encrypt(text));
   } else {
     alert('TEXTAREA VAZIO!!');
   }
 });
 
 DOM.btnDecrypt.addEventListener('click', () => {
-  if (DOM.textarea.value.trim()) {
-    let text = DOM.textarea.value.toLowerCase();
-    decrypt(text);
+  if (DOM.displayResultText.value.trim()) {
+    let text = DOM.displayResultText.value.toLowerCase();
+    showMessage(decrypt(text));
   } else {
-    alert('TEXTAREA VAZIO!!');
+    if (DOM.userText.value.trim()) {
+      let text = DOM.userText.value.toLowerCase();
+      showMessage(decrypt(text));
+    } else {
+      alert('TEXTAREA VAZIO!!');
+    }
   }
 });
